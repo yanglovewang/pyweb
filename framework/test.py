f72f8f48cc9a2a815f41803447253b02fc84be08
yang <yangchengjuily@163.com>
2019-11-16 23:26:55 +0800

    test

--- framework/test.py
+++ framework/test.py
@@ -1,5 +1,6 @@
 
-
 print(len(b"dscds"))
 
 print(b"dscds" + b"dscsd")
+
+print(b"\u5f53\u524d\u7528\u6237\u79ef\u5206\u548cC\u5e01\u4e0d\u8db3,\u8bf7\u524d\u5f80\u5145\u503c".decode(encoding="utf-8"))
